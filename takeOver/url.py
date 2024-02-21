4feca92a59bc967d1ea360f074e6c7d464c9d759
Yamanada Kyogo <lyamanada@kyogray.love>
2024-02-21 11:46:21 +0900

    url

---
+++ takeOver/url.py
@@ -0,0 +1,40 @@
+# RNN 紹介サイト
+https://www.imagazine.co.jp/再帰型ニューラルネットワークの「基礎の基礎」/
+################################################################################
+
+# 天気予報サイト
+https://pe-sawaki.com/WeatherForecast/
+# pandas 文字列'日付'を数値'日付'に変換
+https://itstudio.co/2020/09/05/10989/
+# 天候情報サイト(気象庁)
+https://www.data.jma.go.jp/gmd/risk/obsdl/#
+# 過去の気象情報(比較用)
+https://www.data.jma.go.jp/obd/stats/etrn/index.php?prec_no=44&block_no=47662&year=2020&month=04&day=&view=a2
+################################################################################
+
+
+# 電力使用量
+https://www.tepco.co.jp/forecast/html/download-j.html
+################################################################################
+
+# 多変量LSTM 参考サイト
+https://qiita.com/tizuo/items/b9af70e8cdc7fb69397f
+# その github
+https://github.com/tizuo/keras/blob/master/LSTM%20with%20multi%20variables.ipynb
+# keras で LSTM の複数処理する方法は？
+https://www.it-swarm-ja.com/ja/python/kerasでlstmの複数の入力を処理する方法/829729866/
+# LSTMによるワンステップ予測：ホテルの収益の予測
+https://ichi.pro/lstm-niyoru-wansuteppu-yosoku-hoteru-no-shueki-no-yosoku-222028556595935
+################################################################################
+
+# 相関係数
+https://deepage.net/features/numpy-corrcoef.html
+# 相関係数 2
+https://analysis-navi.com/?p=2751
+# 相関係数 3
+https://sci-pursuit.com/math/statistics/correlation-coefficient.html
+################################################################################
+
+# MinMaxScaler
+https://qiita.com/Qiitaman/items/c94420e8b86aae5f28a9
+################################################################################
